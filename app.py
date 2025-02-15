7e3311426e5033c09734b392a00cbff013d25c76
Krushna Rohitkumar Patel <Krushna.Patel@igtsolutions.com>
2025-02-15 10:20:01 +0530

    my commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-   return "Hello Everyone,This is Krushna's Flask Server!"
+   return "Hello Everyone,This is Krushna's Flask Server Version 1 Added!"
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
